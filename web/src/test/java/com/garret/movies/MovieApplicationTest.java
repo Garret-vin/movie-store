52050341618fd64017ffacc0f48e521443e03aa8
garret <pavlenko.av92@gmail.com>
2019-11-07 14:30:11 +0200

    added test that apptication context is loaded

--- web/src/test/java/com/garret/movies/MovieApplicationTest.java
+++ web/src/test/java/com/garret/movies/MovieApplicationTest.java
@@ -1,23 +1,18 @@
 package com.garret.movies;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+@SpringBootTest(classes = MovieApplication.class)
+@Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class MovieApplicationTest {
-
-    @Autowired
-    private ApplicationContext context;
 
     @Test
     public void initContext() {
-        assertThat(context).isNotNull();
+        log.info("Context initialized");
     }
 }
