e4281109f1d10f8423c1e9427fe327d718276504
garret <pavlenko.av92@gmail.com>
2019-11-26 12:31:45 +0200

    add jooq plugin in pom

--- dao/src/main/java/com/garret/movies/dao/repository/MovieRepository.java
+++ dao/src/main/java/com/garret/movies/dao/repository/MovieRepository.java
@@ -4,6 +4,7 @@ import com.garret.movies.dao.entity.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.util.MultiValueMap;
 
 import java.util.Optional;
 
